9c36bbddcc68642853a519d007bc203454e9dd30
Xavier Francis <james.x.francis9@gmail.com>
2023-04-12 10:53:38 -0500

    title and rating arrays created

--- js/movie-app.js
+++ js/movie-app.js
@@ -1,23 +1,18 @@
 "use strict";
-
-$(window).on('load', function () {
-    fetch('http://localhost:3000/movies')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            let titleArr = [];
-            data.forEach(function(element) {
-                $('#movies').append(`<div class="movieCard"><p class="movieTitle">${element.title}</p><p class="movieRating">${element.rating}</p><button class="editButton">Edit</button><button id="deleteButton">Delete</button></div>`)
-            })
-            for (let i = 0; i < $('.movieCard').length; i++) {
-                titleArr.push($('.editButton:nth-of-type(' + i +')').parent().children('.movieTitle').first().text());
-            }
-            {
-                console.log(titleArr);
-                $('#loading').hide();
-            }});
-
+let titleArr = [];
+let ratingArr = [];
+fetch('http://localhost:3000/movies')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(function(element) {
+            $('#movies').append(`<div class="movieCard"><p class="movieTitle">${element.title}</p><p class="movieRating">${element.rating}</p><button class="editButton">Edit</button><button class="deleteButton">Delete</button></div>`)
+            titleArr.push(element.title);
+            ratingArr.push(element.rating);
+        })
+        $('#loading').hide();
 });
+
 
 $('#addMovieButton').click(function(e) {
     e.preventDefault();
@@ -40,7 +35,15 @@ $('#addMovieButton').click(function(e) {
         .then(data => {
             console.log(data)
             $('#movies').append(`<div class="movieCard"><p class="movieTitle">${data.title}</p><p class="movieRating">${data.rating}</p><button class ="editButton">Edit</button><button class="deleteButton">Delete</button></div>`)
+            titleArr.push(data.title);
+            ratingArr.push(data.rating);
         })
+
         .catch(error => console.error(error));
 });
+
+console.log(titleArr);
+console.log(ratingArr);
+
+
 
